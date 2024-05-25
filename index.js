956ca7f5ba1b566b1f06e30f2b6f3a34912abe9d
ImanBurluk <jaroslavzev63@mail.ru>
2024-05-25 17:26:11 +0500

    Create route access to admins and moderators

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import Datastore from 'nedb-promises';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken'
-import {config} from './config.js'
+import { config } from './config.js'
 
 const users = Datastore.create('User.db');
+// const userRefreshTokens = Datastore.create('UserRefreshTokens.db');
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.get('/', (req, res) => {
 
 app.post('/api/auth/register', async (req, res) => {
 	try {
-		const { name, email, password } = req.body;
+		const { name, email, password, role } = req.body;
 
 		if (!name || !email || !password) {
 			return res.status(422).json({ message: 'Укажите, пожалуйста все поля!' });
@@ -32,6 +33,7 @@ app.post('/api/auth/register', async (req, res) => {
 			name,
 			email,
 			password: hashedPassword,
+			role: role ?? 'member'
 		});
 
 		return res
@@ -42,74 +44,112 @@ app.post('/api/auth/register', async (req, res) => {
 	}
 });
 
-app.post('/api/auth/login', async (req, res)=>{
+app.post('/api/auth/login', async (req, res) => {
 
 	try {
-		
-			const {email, password} = req.body
 
-			if(!email || !password){
-				return res.status(422).json({message:'Please fill in all fields (email and password)'})
-			}
+		const { email, password } = req.body
 
-			
-			const user = await users.findOne({email})
-			
-			if(!user){
-				return res.status(401).json({ message: 'Email or password is invalid' });
-			}
-			
-			const passwordMatch = await bcrypt.compare(password, user.password)
+		if (!email || !password) {
+			return res.status(422).json({ message: 'Please fill in all fields (email and password)' })
+		}
 
-			if(!passwordMatch){
-				return res.status(401).json({ message: 'Email or password is invalid' });
-			}
 
-			const accessToken = jwt.sign({userId:user._id}, config.accessTokenSecret, { subject:'accessApi', expiresIn:'1h'})
+		const user = await users.findOne({ email })
 
-			return res.status(200).json({
-				id: user._id,
-				name:user.name,
-				email: user.email,
-				accessToken
-			})
+		if (!user) {
+			return res.status(401).json({ message: 'Email or password is invalid' });
+		}
+
+		const passwordMatch = await bcrypt.compare(password, user.password)
+
+		if (!passwordMatch) {
+			return res.status(401).json({ message: 'Email or password is invalid' });
+		}
+
+		const accessToken = jwt.sign({ userId: user._id }, config.accessTokenSecret, { subject: 'accessApi', expiresIn: '1h' })
+
+		// const refreshToken = jwt.sign({ userId: user._id }, config.refrehTokenSecret, { subject: 'refreshToken', expiresIn: '1w' })
+
+		// await userRefreshTokens.insert({
+		// 	refreshToken,
+		// 	userId: user._id
+		// })
+
+		return res.status(200).json({
+			id: user._id,
+			name: user.name,
+			email: user.email,
+			accessToken,
+			// refreshToken
+		})
 	} catch (error) {
 		return res.status(500).json({ message: error.message });
 	}
 })
 
-app.get('/api/user/current', ensureAuthenticated, async(req,res)=>{
+// app.post('/api/auth/refresh-token', async (req, res) => {
+
+// })
+
+app.get('/api/user/current', ensureAuthenticated, async (req, res) => {
 	try {
-		const user = await users.findOne({_id:req.user.id})
+		const user = await users.findOne({ _id: req.user.id })
 
 		return res.status(200).json({
 			id: user._id,
 			name: user.name,
-			email:user.email
+			email: user.email
 		})
 
 	} catch (error) {
-		return res.status(401).json({message: error.message})
+		return res.status(401).json({ message: error.message })
 	}
 })
 
-async function ensureAuthenticated (req, res, next){
-		const accessToken = req.headers.authorization
+async function ensureAuthenticated(req, res, next) {
+	const accessToken = req.headers.authorization
 
-		if(!accessToken){
-			return res.status(401).json({message: 'Access token not found'})
+	if (!accessToken) {
+		return res.status(401).json({ message: 'Access token not found' })
+	}
+
+	try {
+		const decodedAccessToken = jwt.verify(accessToken, config.accessTokenSecret)
+
+		req.user = { id: decodedAccessToken.userId }
+
+		next()
+	} catch (error) {
+		return res.status(401).json({ message: 'Access token invalid or expired' })
+	}
+}
+
+app.get('/api/admin', ensureAuthenticated, authorise(['admin']), (req, res) => {
+	return res.status(200).json({ message: 'Only admins can access this route!' })
+})
+
+
+app.get('/api/moderator', ensureAuthenticated, authorise(['admin', 'moderator']), async (req, res) => {
+	try {
+		return res.status(200).json({ message: 'Only admins and moderator can access this route!' })
+
+	} catch (error) {
+		return res.status(401).json({ message: error.message })
+	}
+})
+
+function authorise(roles = []) {
+	return async function (req, res, next) {
+		const user = await users.findOne({ _id: req.user.id })
+
+		if (!user || !roles.includes(user.role)) {
+			return res.status(403).json({ message: 'Access denied' })
 		}
 
-		try {
-			const decodedAccessToken = jwt.verify(accessToken, config.accessTokenSecret)
-
-			req.user = {id:decodedAccessToken.userId}
-
-			next()
-		} catch (error) {
-			return res.status(401).json({message: 'Access token invalid or expired'})
-		}
- }
+		next()
+	}
+}
 
 app.listen(5000, () => {
 	console.log('Server started on port 5000');
